dc6083f52cb588f2462860d6f4a2a2ddb203f1dd
Pham Minh Man <pmmdrg2605@gmail.com>
2023-07-08 16:29:12 +0700

    Finish core knowledge

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3001;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(morgan("combined"));
+
 app.engine(
   ".hbs",
   engine({
@@ -14,14 +21,20 @@ app.engine(
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.use(morgan("combined"));
-
 app.get("/", (req, res) => {
   return res.render("home");
 });
 
 app.get("/news", (req, res) => {
   return res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  return res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  return res.send(req.body);
 });
 
 app.listen(port, () => console.log(`App listening on port: ${port}`));
